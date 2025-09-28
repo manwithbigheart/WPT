1d8e39fa05e2afb3cf46ad1bef622f831c31ca5d
Abdul Rehman Shah <abdulreheman34@gmail.com>
2025-09-28 18:26:10 +0530

    update

--- D7/2CreatingServer/http.js
+++ D7/2CreatingServer/http.js
@@ -1,10 +1,21 @@
 const http = require('http');
 
 let server = http.createServer((req, res) => {
-  res.writeHead("server is running");
-  res.end();
-})
-
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+  if (req.url === '/') {
+    res.write("Server started");
+    res.end();
+  }else if (req.url === '/mobile') {
+    res.write("You'r request for mobile");
+    res.end();
+  } else if (req.url === '/laptop') {
+    res.write("You'r request for laptop");
+    res.end();
+  }
+  else {
+    res.write("Invalid Request");
+    res.end();
+  }
+});
+server.listen(5000,() => {
+  console.log("Server Created at port 5000 Successfully");
 });
